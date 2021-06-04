6e7a1f2721a3409740333ef41a516b84fe12803d
souviksahabhowmik <souviksaha.bhowmik@gfk.com>
2021-06-04 16:06:56 +0530

    corrections

--- odi/retrain/retrain_ci.py
+++ odi/retrain/retrain_ci.py
@@ -165,8 +165,8 @@ def retrain_one_shot_multi(learning_rate=0.001,epoch = 150,batch_size=10,monitor
 
     }
     loss_weights = {
-                       'final_score': 0.25,
-                       'is_win': 10
+                       'final_score': 1,
+                       'is_win': 20000
                     }
 
 
